7daa5f441fcd586d6f907600d8532f0a2f747e47
lsd <lsd@gmu.edu>
2021-08-25 20:12:24 -0400

    JSON Data Added

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HotelInfo from './HoteInfo';
+import Welcome from './Welcome';
+
+const Main = () => (
+  <main id="wrapper">
+      <Welcome />
+      <HotelInfo />
+  </main>
+);
+export default Main;
